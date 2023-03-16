c12c2e44b7edf64b5b3518d8f2e64c839e3c1dbd
Dirk Hartung <dirk.hartung@publicissapient.com>
2023-03-16 17:40:23 +0100

    May try on Exercises: Conditional logic

--- src/main/java/com/serenitydojo/Dog.java
+++ src/main/java/com/serenitydojo/Dog.java
@@ -1,23 +1,18 @@
 package com.serenitydojo;
 
 public class Dog extends Pet {
-    private String favoriteToy;
+
     private boolean isFed = false;
 
     public static final String DOG_NOISE = "Woof";
 
+    private String favoriteToy;
+
     public Dog(String name, String favoriteToy, int age) {
         super(name, age);
-        this.favoriteToy = favoriteToy;
+        setFavoriteToy(favoriteToy);
     }
 
-    public String getFavoriteToy() {
-        return favoriteToy;
-    }
-
-    public void setFavoriteToy(String favoriteToy) {
-        this.favoriteToy = favoriteToy;
-    }
 
     public boolean isFed() {
         return isFed;
@@ -33,5 +28,13 @@ public class Dog extends Pet {
     @Override
     public String play() {
         return "plays with bone";
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
     }
 }
